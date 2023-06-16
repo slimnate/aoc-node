d12c27d1c98857de889628eb20b384be31bd4323
Slim Nate <slimnatekc@gmail.com>
2023-06-15 20:19:20 -0500

    auto-detect each days folder in the src directory as options for the run prompt

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
+import { readdirSync } from 'fs';
 import inquirer from 'inquirer';
+import path from 'path';
 
-inquirer
-  .prompt([
-    {
-      type: 'list',
-      name: 'day',
-      message: 'Choose a day to run',
-      choices: ['01'],
-      filter(val) {
-        return val;
-      },
+const challenges = readdirSync(path.resolve('./src'), { withFileTypes: true })
+  .filter((dirent) => dirent.isDirectory())
+  .map((dirent) => dirent.name);
+
+const questions = [
+  {
+    type: 'list',
+    name: 'day',
+    message: 'Choose a challenge day to run',
+    choices: challenges,
+    filter(val) {
+      return val;
     },
-  ])
-  .then(({ day }) => {
-    import(`./${day}/index.js`).then((module) => {
-      module.run();
-    });
+  },
+];
+
+inquirer.prompt(questions).then(({ day }) => {
+  import(`./${day}/index.js`).then((module) => {
+    module.run();
   });
+});
 
 export {};
