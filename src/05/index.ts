8825ca2e60cfb90102e1b5d5f60fb64832939ed3
SlimNate <slimnatekc@gmail.com>
2023-09-04 21:42:08 -0500

    Add day 5 part 2

--- src/05/index.ts
+++ src/05/index.ts
@@ -1,5 +1,7 @@
 import { readInputForChallenge } from '../helpers.js';
 
+declare type StackArray = Array<Array<string>>;
+
 enum Mode {
   PARSE,
   MANIPULATE,
@@ -17,7 +19,7 @@ class Move {
   to: number;
 }
 
-function parseInput(stacks: Array<Array<string>>, moves: Array<Move>) {
+function parseInput(stacks: StackArray, moves: Array<Move>) {
   const lines = readInputForChallenge('05');
   let mode = Mode.PARSE;
 
@@ -57,7 +59,7 @@ function parseInput(stacks: Array<Array<string>>, moves: Array<Move>) {
   }
 }
 
-function processMoves(stacks: Array<Array<string>>, moves: Array<Move>) {
+function processMovesSingle(stacks: StackArray, moves: Array<Move>) {
   let move: Move | undefined;
   while ((move = moves.shift())) {
     for (let i = 0; i < move.count; i++) {
@@ -69,28 +71,66 @@ function processMoves(stacks: Array<Array<string>>, moves: Array<Move>) {
   }
 }
 
+function processMovesMultiple(stacks: StackArray, moves: Array<Move>) {
+  let move: Move | undefined;
+  while ((move = moves.shift())) {
+    const splitIndex = stacks[move.from].length - move.count;
+
+    // copy crates off top of from stack
+    let crates = stacks[move.from].slice(splitIndex);
+    // remove crates from top of from stack
+    stacks[move.from] = stacks[move.from].slice(0, splitIndex);
+    // add crates to top of to crate
+    stacks[move.to].push(...crates);
+  }
+}
+
+/**
+ * Get all the top crates from `stacks`
+ * @param stacks
+ * @returns String containing the top crate of each stack in `stacks`
+ */
+function getTopLayer(stacks: StackArray) {
+  return stacks.reduce(
+    (res, curr) => res + (curr.at(-1) ? curr.at(-1) : ''),
+    ''
+  );
+}
+
+/**
+ * Copy a 2D array
+ * @param stacks 2D array to copy
+ * @returns copied array
+ */
+function copy2D(stacks: StackArray): StackArray {
+  return stacks.map((stack) => [...stack]);
+}
+
 function run() {
   //initialize stacks with 9 empty arrays
-  let stacks: Array<Array<string>> = [];
+  let stacks: StackArray = [];
+  let stacksMultiple: StackArray;
   let moves: Array<Move> = [];
 
   for (let i = 0; i < 9; i++) {
     stacks[i] = [];
   }
 
+  //parse input and copy for part 2
   parseInput(stacks, moves);
 
-  processMoves(stacks, moves);
+  // can't do shallow copy for stacks because sub-arrays still have same ref
+  stacksMultiple = copy2D(stacks);
 
-  // print answer to part one, which crate is at the top of each stack
-  const topLayer = stacks.reduce(
-    (res, curr) => res + (curr.at(-1) ? curr.at(-1) : ''),
-    ''
-  );
+  // part 1
+  processMovesSingle(stacks, [...moves]); // using spread to create shallow copy
+  let topLayer = getTopLayer(stacks);
+  console.log(`Part 1: ${topLayer}`);
 
-  console.log(topLayer);
-
-  // console.log(stacks);
+  // part 2
+  processMovesMultiple(stacksMultiple, [...moves]); // using spread to create shallow copy
+  topLayer = getTopLayer(stacksMultiple);
+  console.log(`Part 2: ${topLayer}`);
 }
 
 export { run };
